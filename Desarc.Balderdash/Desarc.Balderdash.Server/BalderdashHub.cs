e9cec5f0b84d6e899e6ef48d45d0423886977397
Øyvin <desarc00@gmail.com>
2015-02-09 22:42:38 +0100

    initial server commit, simple signalR hub

---
+++ Desarc.Balderdash/Desarc.Balderdash.Server/BalderdashHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Desarc.Balderdash.Server
+{
+    public class BalderdashHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
